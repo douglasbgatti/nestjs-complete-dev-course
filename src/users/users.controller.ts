bf225ea2ca7233651a2f603524f8fb7d48f1c9a0
Douglas Gatti <douglasbgatti@gmail.com>
2023-02-11 16:02:08 -0300

    Create users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import { Serialize } from '../interceptors/serialize.interceptors';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UserDto } from './dtos/user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto) {
+    await this.usersService.createUser(body);
+  }
+
+  @Get('/users')
+  async findAllUsers() {
+    return this.usersService.findAllUsers();
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findUser(id);
+
+    if (!user) {
+      throw new NotFoundException(`User not found with id: ${id}`);
+    }
+
+    return user;
+  }
+
+  @Delete('/:id')
+  async deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    await this.usersService.update(id, body);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  async createUser(userDto: CreateUserDto): Promise<User> {
+    const user = this.repository.create({ ...userDto });
+
+    return await this.repository.save(user);
+  }
+
+  async findUser(id: string): Promise<User | null> {
+    return this.repository.findOneBy({ id });
+  }
+
+  async findAllUsers(): Promise<User[]> {
+    return this.repository.find();
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.repository.findOneBy({ email });
+  }
+
+  async update(id: string, updatedUser: Partial<User>): Promise<User> {
+    const user = await this.findUser(id);
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found)`);
+    }
+
+    Object.assign(user, updatedUser);
+
+    return this.repository.save(user);
+  }
+
+  async remove(id: string): Promise<User> {
+    const user = await this.findUser(id);
+
+    if (!user) {
+      throw new NotFoundException(`User ${id} not found)`);
+    }
+
+    return this.repository.remove(user);
+  }
+}
